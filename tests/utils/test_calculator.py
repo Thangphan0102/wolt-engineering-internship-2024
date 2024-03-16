15a2bb08638a7aaab48d86da3ac18d3935179b79
Thang Phan <thang.phancao@aalto.fi>
2024-03-16 22:20:02 +0200

    Refactor

--- tests/utils/test_calculator.py
+++ tests/utils/test_calculator.py
@@ -38,9 +38,9 @@ cases_4 = [
 # is_free_delivery
 cases_5 = [
     {"cart_value": 0, "expected": False},
-    {"cart_value": 10000, "expected": False},
+    {"cart_value": 6215, "expected": False},
     {"cart_value": 20000, "expected": True},
-    {"cart_value": 25000, "expected": True},
+    {"cart_value": 26215, "expected": True},
 ]
 
 # calculate_rush_hour_surcharge
@@ -48,115 +48,122 @@ cases_6 = [
     {
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
         "delivery_fee": 1500,
-        "expected": 1500,
+        "expected": 0,
     },
     {
         "time": datetime.datetime(2024, 3, 15, 8, 45, 34),
         "delivery_fee": 1246,
-        "expected": 1246,
+        "expected": 0,
     },
     {
         "time": datetime.datetime(2024, 3, 15, 18, 20, 54),
         "delivery_fee": 1100,
-        "expected": 1320,
+        "expected": 220,
     },
     {
-        "time": datetime.datetime(2024, 3, 14, 18, 20, 54),
+        "time": datetime.datetime(2024, 3, 15, 20, 20, 54),
         "delivery_fee": 1100,
-        "expected": 1100,
+        "expected": 0,
     },
 ]
 
 # calculate_delivery_fee
 cases_7 = [
     {
-        "delivery_distance": 500,
         "cart_value": 900,
+        "delivery_distance": 500,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1200,
+        "expected": 300,
     },
     {
-        "delivery_distance": 500,
         "cart_value": 1100,
+        "delivery_distance": 500,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1300,
+        "expected": 200,
     },
     {
+        "cart_value": 800,
         "delivery_distance": 1100,
-        "cart_value": 800,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1300,
+        "expected": 500,
     },
     {
+        "cart_value": 800,
         "delivery_distance": 1560,
-        "cart_value": 800,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1400,
+        "expected": 600,
     },
     {
+        "cart_value": 800,
         "delivery_distance": 2050,
-        "cart_value": 800,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1500,
+        "expected": 700,
     },
     {
-        "delivery_distance": 570,
         "cart_value": 900,
+        "delivery_distance": 570,
         "number_of_items": 5,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1250,
+        "expected": 350,
     },
     {
-        "delivery_distance": 570,
         "cart_value": 900,
+        "delivery_distance": 570,
         "number_of_items": 10,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1500,
+        "expected": 600,
     },
     {
-        "delivery_distance": 570,
         "cart_value": 800,
+        "delivery_distance": 570,
         "number_of_items": 14,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
-        "expected": 1500,
+        "expected": 1020,
     },
     {
-        "delivery_distance": 540,
         "cart_value": 20000,
+        "delivery_distance": 540,
         "number_of_items": 12,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
         "expected": 0,
     },
     {
-        "delivery_distance": 460,
         "cart_value": 25000,
+        "delivery_distance": 460,
         "number_of_itesm": 4,
         "time": datetime.datetime(2024, 3, 14, 8, 0, 0),
         "expected": 0,
     },
     {
-        "delivery_distance": 500,
         "cart_value": 900,
+        "delivery_distance": 500,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 15, 8, 0, 0),
-        "expected": 1200,
+        "expected": 300,
     },
     {
-        "delivery_distance": 500,
         "cart_value": 900,
+        "delivery_distance": 500,
         "number_of_items": 2,
         "time": datetime.datetime(2024, 3, 15, 15, 0, 0),
-        "expected": 1440,
+        "expected": 360,
     },
     {
-        "delivery_distance": 1560,
         "cart_value": 900,
+        "delivery_distance": 1560,
         "number_of_items": 2,
+        "time": datetime.datetime(2024, 3, 15, 18, 39, 12),
+        "expected": 600,
+    },
+    {
+        "cart_value": 860,
+        "delivery_distance": 3050,
+        "number_of_items": 14,
         "time": datetime.datetime(2024, 3, 15, 18, 39, 12),
         "expected": 1500,
     },
@@ -173,7 +180,9 @@ def test_calculate_distance_surcharge(fee_calculator, distance_surcharge):
     delivery_distance = distance_surcharge.get("delivery_distance")
     expected_surcharge = distance_surcharge.get("expected")
     assert (
-        fee_calculator.calculate_distance_surcharge(delivery_distance=delivery_distance)
+        fee_calculator._calculate_distance_surcharge(
+            delivery_distance=delivery_distance
+        )
         == expected_surcharge
     )
 
@@ -183,7 +192,7 @@ def test_calculate_cart_value_surcharge(fee_calculator, cart_value_surcharge):
     cart_value = cart_value_surcharge.get("cart_value")
     expected_surcharge = cart_value_surcharge.get("expected")
     assert (
-        fee_calculator.calculate_cart_value_surcharge(cart_value=cart_value)
+        fee_calculator._calculate_cart_value_surcharge(cart_value=cart_value)
         == expected_surcharge
     )
 
@@ -193,7 +202,7 @@ def test_calculate_number_of_items_surcharge(fee_calculator, item_surcharge):
     number_of_items = item_surcharge.get("number_of_items")
     expected_surcharge = item_surcharge.get("expected")
     assert (
-        fee_calculator.calculate_item_surcharge(number_of_items=number_of_items)
+        fee_calculator._calculate_item_surcharge(number_of_items=number_of_items)
         == expected_surcharge
     )
 
@@ -202,14 +211,14 @@ def test_calculate_number_of_items_surcharge(fee_calculator, item_surcharge):
 def test_limit_delivery_fee(fee_calculator, limit_delivery_fee):
     delivery_fee = limit_delivery_fee.get("delivery_fee")
     expected_fee = limit_delivery_fee.get("expected")
-    assert fee_calculator.limit_delivery_fee(delivery_fee=delivery_fee) == expected_fee
+    assert fee_calculator._limit_delivery_fee(delivery_fee=delivery_fee) == expected_fee
 
 
 @pytest.mark.parametrize("is_free_delivery", cases_5)
 def test_is_free_delivery(fee_calculator, is_free_delivery):
     cart_value = is_free_delivery.get("cart_value")
     expected_result = is_free_delivery.get("expected")
-    assert fee_calculator.is_free_delivery(cart_value=cart_value) == expected_result
+    assert fee_calculator._is_free_delivery(cart_value=cart_value) == expected_result
 
 
 @pytest.mark.parametrize("calculate_rush_hour_surcharge", cases_6)
@@ -218,7 +227,7 @@ def test_calculate_rush_hour_surcharge(fee_calculator, calculate_rush_hour_surch
     delivery_fee = calculate_rush_hour_surcharge.get("delivery_fee")
     expected_surcharge = calculate_rush_hour_surcharge.get("expected")
     assert (
-        fee_calculator.calculate_rush_hour_surcharge(
+        fee_calculator._calculate_rush_hour_surcharge(
             time=time, delivery_fee=delivery_fee
         )
         == expected_surcharge
