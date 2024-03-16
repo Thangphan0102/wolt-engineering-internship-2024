826a913472a4417d274da8aca3e3a180e574fb4d
Thang Phan <thang.phancao@aalto.fi>
2024-03-16 22:59:26 +0200

    Modify validations

--- app/schemas/fees.py
+++ app/schemas/fees.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel, Field
 
 
 class FeeCalculatorRequest(BaseModel):
-    cart_value: int = Field(ge=0, description="Value of the shopping cart in cents")
+    cart_value: int = Field(gt=0, description="Value of the shopping cart in cents")
     delivery_distance: int = Field(
-        ge=0,
+        gt=0,
         description="The distance between the store and customer’s location in meters",
     )
     number_of_items: int = Field(
-        ge=0, description="The number of items in the customer's shopping cart"
+        gt=0, description="The number of items in the customer's shopping cart"
     )
     time: datetime.datetime = Field(description="Order time in UTC in ISO format")
     model_config = {
